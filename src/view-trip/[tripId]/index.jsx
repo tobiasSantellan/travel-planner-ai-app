16b0b6e688d52e6e8abb3be70cf9c77208ea5c81
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-20 00:44:59 -0300

    Creation of folder and files to manage dynamic routes

---
+++ src/view-trip/[tripId]/index.jsx
@@ -0,0 +1,43 @@
+import { db } from "@/service/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import InfoSection from "../components/InfoSection";
+
+function ViewTrip() {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState();
+
+  useEffect(() => {
+    tripId && GetTripData();
+  }, [tripId]);
+
+  // used to get trip information from Firebase
+  const GetTripData = async () => {
+    const docRef = doc(db, "AITrips", tripId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setTrip(docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+      toast("No Trip Found");
+    }
+  };
+  return (
+    <div className="p-10 md:px-20 lg:px-44 xl:px-56">
+      {/* information section */}
+      <InfoSection trip={trip} />
+      {/* recommended hotels */}
+
+      {/* daily plan */}
+
+      {/* footer */}
+    </div>
+  );
+}
+
+export default ViewTrip;
---
+++ src/view-trip/components/InfoSection.jsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { IoIosSend } from "react-icons/bs";
+
+function InfoSection({ trip }) {
+  return (
+    <div>
+      <img
+        src="/placeholder.jpg"
+        className="h-[340px] w-full object-cover rounded-xl"
+      />
+      <div className="flex justify-between items-center">
+        <div className="my-5 flex flex-col gap-2">
+          <h2 className="font-bold text-2xl">
+            {" "}
+            {trip?.userSelection?.location?.label}
+          </h2>
+          <div className="flex gap-5">
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
+              📅{trip?.userSelection?.noOfDays} Days
+            </h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
+              💲{trip?.userSelection?.budget} Budget
+            </h2>
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
+              🥂 No. Of Traveler: {trip?.userSelection?.traveler}
+            </h2>
+          </div>
+        </div>
+      </div>
+      <Button>
+        <IoIosSend />
+      </Button>
+    </div>
+  );
+}
+
+export default InfoSection;
