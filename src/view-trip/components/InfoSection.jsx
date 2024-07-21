c7add827b7280aba8d7f351409720fa5ff13ac16
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-07-20 23:39:05 -0300

    logic for image rendering

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { GetPlaceDetails, PHOTO_REF_URL } from "@/service/GlobalApi";
+import { useEffect, useState } from "react";
 import { IoIosSend } from "react-icons/io";
 
 function InfoSection({ trip }) {
+  const [photoUrl, setPhotoUrl] = useState();
+  useEffect(() => {
+    trip && GetPlacePhoto();
+  }, [trip]);
+
+  const GetPlacePhoto = async () => {
+    const data = {
+      textQuery: trip?.userSelection?.location?.label,
+    };
+    const result = await GetPlaceDetails(data).then((resp) => {
+      console.log(resp.data.places[0].photos[3].name);
+
+      const PhotoUrl = PHOTO_REF_URL.replace(
+        "{NAME}",
+        resp.data.places[0].photos[3].name
+      );
+      setPhotoUrl(PhotoUrl);
+    });
+  };
   return (
     <div>
       <img
-        src="/placeholder.jpg"
+        src={photoUrl}
         className="h-[340px] w-full object-cover rounded-xl"
       />
       <div className="flex justify-between items-center">
@@ -15,21 +36,21 @@ function InfoSection({ trip }) {
             {trip?.userSelection?.location?.label}
           </h2>
           <div className="flex gap-5">
-            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
-              📅 {trip?.userSelection?.noOfDays} Days and years
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text:xs md:text-md">
+              📅 {trip?.userSelection?.noOfDays} Days
             </h2>
-            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text:xs md:text-md">
               💲 {trip?.userSelection?.budget} Budget
             </h2>
-            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500">
+            <h2 className="p-1 px-3 bg-gray-200 rounded-full text-gray-500 text:xs md:text-md">
               🥂 No. Of Traveler: {trip?.userSelection?.traveler}
             </h2>
           </div>
         </div>
+        {/* <Button>
+          <IoIosSend />
+        </Button> */}
       </div>
-      <Button>
-        <IoIosSend />
-      </Button>
     </div>
   );
 }
